60985cd5c110cc219f6e17aedcfbf50a46d45844
Kunal-Kumar-Jaiswal <86217510+Kunal-Kumar-Jaiswal@users.noreply.github.com>
2022-03-24 16:57:17 +0530

    Create Pattern_13.java

---
+++ Patterns/Pattern_13.java
@@ -0,0 +1,26 @@
+1	
+1	1	
+1	2	1	
+1	3	3	1	
+1	4	6	4	1
+  
+//it is printing nCr value and we know nC(k+1) = (nCk * (n-k)) / (k+1)
+  
+  import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        
+        for(int i=0;i<n;i++) {
+            int prev = 1;
+            for(int j=0;j<=i;j++) {
+                System.out.print(prev + "\t");
+                prev = (prev * (i-j)) / (j+1);
+            }
+            System.out.println();
+        }
+        
+    }
+}
