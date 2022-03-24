27b9091a8a3304e4512dc6d7a85cb55c4faa8050
Kunal-Kumar-Jaiswal <86217510+Kunal-Kumar-Jaiswal@users.noreply.github.com>
2022-03-24 16:16:20 +0530

    Create Pattern_12.java

---
+++ Patterns/Pattern_12.java
@@ -0,0 +1,31 @@
+0	
+1	1	
+2	3	5	
+8	13	21	34	
+55	89	144	233	377
+  
+//It seems, we have to print fabonacci series
+  
+  import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int a = 0;
+        int b = 1;
+        
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=i;j++) {
+                System.out.print(a + "\t");
+                int temp = a+b;
+                a = b;
+                b = temp;
+            }
+            System.out.println();
+        }
+
+    }
+}
