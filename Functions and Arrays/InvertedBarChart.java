d6b5c10f32336816a7dbcf5f99b0cf5c2f355764
Kunal-Kumar-Jaiswal <86217510+Kunal-Kumar-Jaiswal@users.noreply.github.com>
2022-05-15 16:15:58 +0530

    Create InvertedBarChart.java

---
+++ Functions and Arrays/InvertedBarChart.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    int max = -1;
+    for(int i=0;i<n;i++) {
+        arr[i] = sc.nextInt();
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    
+    for(int i=0;i<max;i++) {
+        for(int j=0;j<n;j++) {
+            if(arr[j] > i) {
+                System.out.print("*\t");
+            }else {
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+    }
+  }
+
+}
